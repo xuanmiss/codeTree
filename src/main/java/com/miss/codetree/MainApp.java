17c4a730bb782c5ae7e5ec591777bec943ad63e1
xuanmiss <xuanmine@gmail.com>
2021-10-12 18:07:48 +0800

    build(pom): 修改打包方式为 Non-modular 模式，使用shade插件打包

---
+++ src/main/java/com/miss/codetree/MainApp.java
@@ -0,0 +1,16 @@
+package com.miss.codetree;
+
+/**
+ * @project: CodeTree
+ * @package: com.miss.codetree
+ * @author: miss
+ * @date: 2021/10/12 17:55
+ * @since:
+ * @history: 1.2021/10/12 created by miss
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+        CodeTreeApplication.main(args);
+    }
+}
