a7c0c2730821177f99161eb9f1d5683999e5e757
xuanmiss <xuanmine@gmail.com>
2022-08-29 19:06:00 +0800

    feat: add git project read util

---
+++ src/main/java/com/miss/codetree/utils/FileUtil.java
@@ -0,0 +1,49 @@
+package com.miss.codetree.utils;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * @author xuanmiss
+ */
+public class FileUtil {
+
+    public static void main(String[] args) {
+        listDirAndCodeProject("/Users/xuanmiss/code_space/ideaProjects");
+    }
+
+    private static void listDirAndCodeProject(String s) {
+        File f = new File(s);
+        LinkedList<File> dirList = new LinkedList<>();
+        if (f.isDirectory()) {
+            findSubDir(f, dirList);
+        }else {
+            throw new RuntimeException("this is not a dir!!!");
+        }
+        Iterator<File> iterable = dirList.iterator();
+        while (iterable.hasNext()) {
+            System.out.println(iterable.next().getAbsolutePath());
+        }
+    }
+
+    private static void findSubDir(File rootDir, LinkedList<File> dirList) {
+        File[] files = rootDir.listFiles();
+        for(int i = 0; i < files.length; i++ ) {
+            File f = files[i];
+            if (isGitDir(f)) {
+                dirList.add(f);
+                continue;
+            }
+            if (files[i].isDirectory()) {
+                findSubDir(files[i], dirList);
+            }
+
+        }
+    }
+
+    private static boolean isGitDir(File f) {
+        return f.isDirectory() && Arrays.asList(f.list()).contains(".git");
+    }
+}
