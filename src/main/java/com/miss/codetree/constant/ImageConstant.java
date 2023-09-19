2a638d3a5ca08f3184cfb384caff17d93c279639
xuanmiss <xuanmine@gmail.com>
2023-09-19 09:50:00 +0800

    feat: 增加程序安全和代码规范的feat

--- src/main/java/com/miss/codetree/constant/ImageConstant.java
+++ src/main/java/com/miss/codetree/constant/ImageConstant.java
@@ -3,6 +3,8 @@ package com.miss.codetree.constant;
 import com.miss.codetree.CodeTreeApplication;
 import javafx.scene.image.Image;
 
+import java.util.Objects;
+
 /**
  * @project: CodeTree
  * @package: com.miss.codetree.constant
@@ -18,25 +20,25 @@ public class ImageConstant {
         throw new RuntimeException("can't init this class");
     }
 
-    public static final Image addImage = new Image(CodeTreeApplication.class.getResourceAsStream("/icon/add.png"));
+    public static final Image addImage = new Image(Objects.requireNonNull(CodeTreeApplication.class.getResourceAsStream("/icon/add.png")));
 
-    public static final Image pressedAddImage = new Image(CodeTreeApplication.class.getResourceAsStream("/icon/pressedAdd.png"));
+    public static final Image pressedAddImage = new Image(Objects.requireNonNull(CodeTreeApplication.class.getResourceAsStream("/icon/pressedAdd.png")));
 
-    public static final Image foldImage = new Image(CodeTreeApplication.class.getResourceAsStream("/icon/foldImage.png"));
+    public static final Image foldImage = new Image(Objects.requireNonNull(CodeTreeApplication.class.getResourceAsStream("/icon/foldImage.png")));
 
-    public static final Image expandImage = new Image(CodeTreeApplication.class.getResourceAsStream("/icon/expandImage.png"));
+    public static final Image expandImage = new Image(Objects.requireNonNull(CodeTreeApplication.class.getResourceAsStream("/icon/expandImage.png")));
 
-    public static final Image projectImage = new Image(CodeTreeApplication.class.getResourceAsStream("/icon/project.png"));
+    public static final Image projectImage = new Image(Objects.requireNonNull(CodeTreeApplication.class.getResourceAsStream("/icon/project.png")));
 
-    public static final Image openDirImage = new Image(CodeTreeApplication.class.getResourceAsStream("/icon/opendirImage.png"));
+    public static final Image openDirImage = new Image(Objects.requireNonNull(CodeTreeApplication.class.getResourceAsStream("/icon/opendirImage.png")));
 
-    public static final Image presOpenDirImage = new Image(CodeTreeApplication.class.getResourceAsStream("/icon/presOpenDirImage.png"));
+    public static final Image presOpenDirImage = new Image(Objects.requireNonNull(CodeTreeApplication.class.getResourceAsStream("/icon/presOpenDirImage.png")));
 
-    public static final Image expandTreeImage = new Image(CodeTreeApplication.class.getResourceAsStream("/icon/expandTree.png"));
+    public static final Image expandTreeImage = new Image(Objects.requireNonNull(CodeTreeApplication.class.getResourceAsStream("/icon/expandTree.png")));
 
-    public static final Image packupTreeImage = new Image(CodeTreeApplication.class.getResourceAsStream("/icon/packupTree.png"));
+    public static final Image packupTreeImage = new Image(Objects.requireNonNull(CodeTreeApplication.class.getResourceAsStream("/icon/packupTree.png")));
 
-    public static final Image clearTreeImage = new Image(CodeTreeApplication.class.getResourceAsStream("/icon/clear.png"));
+    public static final Image clearTreeImage = new Image(Objects.requireNonNull(CodeTreeApplication.class.getResourceAsStream("/icon/clear.png")));
 
-    public static final Image codeTreeImage = new Image(CodeTreeApplication.class.getResourceAsStream("/icon/codeTree-blue.png"));
+    public static final Image codeTreeImage = new Image(Objects.requireNonNull(CodeTreeApplication.class.getResourceAsStream("/icon/codeTree-blue.png")));
 }
